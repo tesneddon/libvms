a1709661cc3bfee0999f37752211c2c252895d28
Tim E. Sneddon <tim@sneddon.id.au>
2013-08-12 13:24:58 +0800

    Add shell for time.c.  Complete libutc first, as we will use that library.

---
+++ time.c
@@ -0,0 +1,10 @@
+/*++
+**
+**--
+*/
+int sys$gettim(struct _generic_64 *timadr) {
+
+    // fetch the current localtime and convert it to an OpenVMS system time
+    //  -- use the utc services to do this.
+}
+
